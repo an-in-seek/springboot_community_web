523796a127d7c282c5354b7ac29ec7841011df54
an-in-seek <dktmrorl@naver.com>
2020-07-10 22:03:25 +0900

    뷰스타그램 소스 원상복구

---
+++ src/frontend/src/data/posts.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        postNo: 1,
+        username: "user01",
+        postTitle: "Title1",
+        postContent: "When you're ready for summer '18 ☀️",
+        postImage: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/tropical_beach.jpg",
+        like: 20,
+        dislike: 10,
+        createdDate: '2020-07-09 11:29:00'
+    },
+    {
+        postNo: 2,
+        username: "user02",
+        postTitle: "Title2",
+        postContent: "Views from the six...",
+        postImage: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/downtown.jpg",
+        like: 20,
+        dislike: 10,
+        createdDate: '2020-07-09 11:30:00'
+    },
+    {
+        postNo: 3,
+        username: "user03",
+        postTitle: "Title3",
+        postContent: "Current mood 🐶",
+        postImage: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/puppers.jpg",
+        like: 20,
+        dislike: 10,
+        createdDate: '2020-07-09 11:31:00'
+    }
+  ];
---
+++ src/frontend/src/models/post.js
@@ -0,0 +1,14 @@
+
+export default class Post {
+    constructor(postNo, username, postTitle, postContent, postImage, like, dislike, createdDate, comments) {
+      this.postNo = postNo;
+      this.username = username;
+      this.postTitle = postTitle;
+      this.postContent = postContent;
+      this.postImage = postImage;
+      this.like = like;
+      this.dislike = dislike;
+      this.createdDate = createdDate;
+      this.comments = comments;
+    }
+  }
