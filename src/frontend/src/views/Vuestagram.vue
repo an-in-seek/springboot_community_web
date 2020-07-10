1de06ea92fdd54466975e725fd89196ed2bf92d1
hgm9157 <55440244+hgm9157@users.noreply.github.com>
2020-07-10 09:48:40 +0900

    Revert "Merge branch 'master' of https://github.com/an-in-seek/spring_boot_community_web"
    This reverts commit 75bf26857983c7717486a53fc976c465088a2047, reversing
    changes made to fce9b31905d68e9ff0f3e9527c8d2f425e2deb62.

--- src/frontend/src/views/Vuestagram.vue
+++ src/frontend/src/views/Vuestagram.vue
@@ -1,7 +1,45 @@
 <template>
   <div>
     <div class="mt-4">
-        <post v-for="post in posts" :post="post" :key="posts.indexOf(post)"></post>
+        <b-card style="max-width: 30rem;">
+          <font-awesome-icon icon="user"/> user
+        </b-card>
+        <b-card-group deck>
+            <b-card
+            title="제목"
+            img-src="https://picsum.photos/300/300/?image=41"
+            img-alt="Image"
+            img-top
+            style="max-width: 30rem;">
+                <template v-slot:header>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </template>
+
+                <b-card-text>
+                    This is a wider card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    This is a wider card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                </b-card-text>
+
+                <b-button href="#" variant="danger"><font-awesome-icon icon="heart"/> 999</b-button> 
+                <b-button href="#" variant="secondary"><font-awesome-icon icon="heart-broken"/> 999</b-button>
+                <b-button href="#" variant="success"><font-awesome-icon icon="comment"/> 999</b-button>
+                <b-button href="#" variant="primary"><font-awesome-icon icon="share-alt"/></b-button>
+                <div style="float:right;"><b-button href="#" variant="outline-secondary"><font-awesome-icon icon="ellipsis-h"/></b-button></div>
+
+                <template v-slot:footer>
+                  <b-form-group label-for="filterInput" class="mb-0">
+                    <b-input-group>
+                        <b-form-input placeholder="댓글을 입력하세요."></b-form-input>
+                        <b-input-group-append>
+                            <b-button href="#" variant="info" @click="filter = ''">등록</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                  </b-form-group>
+                </template>
+            </b-card>
+        </b-card-group>
     </div>
     <div class="pt-4 mt-4">
       <hr />
@@ -13,19 +51,16 @@
 </template>
 
 <script>
-import posts from "../data/posts"; // mock 데이터 -> 추후 DB에서 데이터 가져오도록 수정
 import UserService from '../services/user.service';
-import Post from '../views/Post';
 
 export default {
-  name: 'Vuestagram',
+  name: 'Home',
   data() {
     return {
-      posts
+      content: '',
+      slide: 0,
+      sliding: null
     };
-  },
-  components: {
-    post: Post
   },
   mounted() {
     UserService.getPublicContent().then(
@@ -39,6 +74,14 @@ export default {
           error.toString();
       }
     );
+  },
+  methods : {
+    onSlideStart() {
+        this.sliding = true
+    },
+    onSlideEnd() {
+        this.sliding = false
+    }
   }
 };
 </script>
