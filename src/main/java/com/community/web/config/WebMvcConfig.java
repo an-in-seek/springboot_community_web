b1e41db8f8b46736f34a02cfde7631fd2bedb80c
an-in-seek <dktmrorl@naver.com>
2020-07-31 21:39:22 +0900

    OS 구분 코드 추가

--- src/main/java/com/community/web/config/WebMvcConfig.java
+++ src/main/java/com/community/web/config/WebMvcConfig.java
@@ -6,19 +6,31 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import com.community.web.util.CommonUtil;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-	
+
 	@Value("${upload.path.image.post}")
-	String uploadPostImagePath;
-	
+	private String uploadPostImagePath;
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry
-		.addResourceHandler(uploadPostImagePath + "/**")
-		.addResourceLocations("file://" + uploadPostImagePath)
-		.setCachePeriod(3600)
-		.resourceChain(true)
-		.addResolver(new PathResourceResolver());
+		boolean isWindows = CommonUtil.isWindows();
+		if(isWindows) {
+			registry
+			.addResourceHandler(uploadPostImagePath + "/**")
+			.addResourceLocations("file://" + uploadPostImagePath)
+			.setCachePeriod(3600)
+			.resourceChain(true)
+			.addResolver(new PathResourceResolver());
+		} else {
+			registry
+			.addResourceHandler(uploadPostImagePath + "/**")
+			.addResourceLocations(uploadPostImagePath)
+			.setCachePeriod(3600)
+			.resourceChain(true)
+			.addResolver(new PathResourceResolver());
+		}
 	}
 }
--- src/main/java/com/community/web/util/CommonUtil.java
+++ src/main/java/com/community/web/util/CommonUtil.java
@@ -2,7 +2,37 @@ package com.community.web.util;
 
 public class CommonUtil {
 
+	/**
+	 * null 여부
+	 * @param obj
+	 * @return
+	 */
 	static public boolean isNull(Object obj) {
 		return obj == null ? true : false;
+	}
+
+	/**
+	 * Windows 여부
+	 * @return
+	 */
+	public static boolean isWindows() {
+		return (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0);
+	}
+
+	/**
+	 * Mac 여부
+	 * @return
+	 */
+	public static boolean isMac() {
+		return (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0);
+	}
+
+	/**
+	 * Unix 여부
+	 * @return
+	 */
+	public static boolean isUnix() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		return (osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0 || osName.indexOf("aix") > 0);
 	}
 }
